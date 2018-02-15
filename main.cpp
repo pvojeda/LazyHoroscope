e9ada8143eaba683727f8293aa6c517ce349fee8
Pvojeda <pojeda@mail.csuchico.edu>
2018-02-15 11:58:01 -0800

    LazyHoroscope

--- main.cpp
+++ main.cpp
@@ -20,28 +20,25 @@ cin>>last;
 cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"."",here is your fortune..."<<endl;
 //tell fortune
 
-cout<<"your lucky number is "<<first.length()+last.length()<<endl;
+cout<<"your lucky number is "<<first.length()<<endl;
 
 char a, e, i, o, u;
 
-if ((first[0]==( a || e || i || o || u))
+if (first[0]== 'a' ||first[0] == 'A' ||first[0] == 'e' ||first[0] == 'E' ||first[0] == 'i' ||first[0] == 'I' ||first[0] == 'o' ||first[0] == 'O' ||first[0] == 'u' ||first[0] == 'U')
 {
 cout<<"You are destined to be famous!\n";
 }
-if ((last[0]==( a || e || i || o || u))
-{
-cout<<"you have already met your true love.\n";
-}
-else 
+else
 {
 cout<<"you should keep a low profile\n";
 }
-string word;
-cin>> word;
-cout<< word[0]<<endl;
-int len=word.length();
-if char first=word[0];
-if (word[ len-1]=(a || e || i || o || u);
+
+int len=last.length();
+char let = last[len-1];
+if (let == 'a' ||let == 'A' ||let == 'e' ||let == 'E' ||let == 'i' ||let == 'I' ||let == 'o' ||let == 'O' ||let == 'u' ||let == 'U')
+{ '
+cout<<"you have already met your true love.\n";
+}
 cout<<"have a good day!"<<endl;
   return 0;
 }
