1552116acd41010633460ba12a25474ef65fa1ef
Pvojeda <pojeda@mail.csuchico.edu>
2018-02-14 18:32:43 -0800

    notdne

--- main.cpp
+++ main.cpp
@@ -20,6 +20,28 @@ cin>>last;
 cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"."",here is your fortune..."<<endl;
 //tell fortune
 
-cout<<"Your lucky number is "<<first.length()+last.length()<<endl;
+cout<<"your lucky number is "<<first.length()+last.length()<<endl;
+
+char a, e, i, o, u;
+
+if ((first[0]==( a || e || i || o || u))
+{
+cout<<"You are destined to be famous!\n";
+}
+if ((last[0]==( a || e || i || o || u))
+{
+cout<<"you have already met your true love.\n";
+}
+else 
+{
+cout<<"you should keep a low profile\n";
+}
+string word;
+cin>> word;
+cout<< word[0]<<endl;
+int len=word.length();
+if char first=word[0];
+if (word[ len-1]=(a || e || i || o || u);
+cout<<"have a good day!"<<endl;
   return 0;
 }
