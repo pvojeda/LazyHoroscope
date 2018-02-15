5e225c6939c4b05cf3fdc042097b31d5ab8d9f7f
Pvojeda <pojeda@mail.csuchico.edu>
2018-02-15 12:24:56 -0800

    LazyHoroscope

--- main.cpp
+++ main.cpp
@@ -17,16 +17,16 @@ cin>>last;
 
   //get user input
 
-cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"."",here is your fortune..."<<endl;
+cout<<"Welcome, " <<first [0]<<"." <<last [0]<<"."", here is your fortune..." <<endl;
 //tell fortune
 
 cout<<"your lucky number is "<<first.length()<<endl;
 
-char a, e, i, o, u;
+
 
 if (first[0]== 'a' ||first[0] == 'A' ||first[0] == 'e' ||first[0] == 'E' ||first[0] == 'i' ||first[0] == 'I' ||first[0] == 'o' ||first[0] == 'O' ||first[0] == 'u' ||first[0] == 'U')
 {
-cout<<"You are destined to be famous!\n";
+cout<<"you are destined to be famous!\n";
 }
 else
 {
