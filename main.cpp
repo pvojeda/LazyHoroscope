024c89e79f99fe9201d9193126039096ac01768e
Pvojeda <pojeda@mail.csuchico.edu>
2018-02-15 17:31:44 -0800

    LazyHoroscope

--- main.cpp
+++ main.cpp
@@ -22,7 +22,6 @@ cout<<"Welcome, " <<first [0]<<"." <<last [0]<<"."", here is your fortune..." <<
 
 cout<<"your lucky number is "<<first.length()<<endl;
 
-# if (first[0] == 'a') || (first[0] == 'A') || ...
 
 if ((first[0]== 'a') || (first[0] == 'A') || (first[0] == 'e') || (first[0] == 'E') || (first[0] == 'i') || (first[0] == 'I') || (first[0] == 'o') || (first[0] == 'O') || (first[0] == 'u') || (first[0] == 'U'))
 {
